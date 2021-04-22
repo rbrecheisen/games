d27f8de96ebeb59e367eb6d3ae3587472fc8be1c
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-04-22 14:24:29 +0200

    first commit

---
+++ src/helloworld.py
@@ -0,0 +1,26 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+
+
+background_image = 'sushiplate.png'
+mouse_image = 'fugu.png'
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480), 0, 32)
+pygame.display.set_caption('Hello, world!')
+
+background = pygame.image.load(background_image).convert()
+mouse_cursor = pygame.image.load(mouse_image).convert_alpha()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            exit()
+    screen.blit(background, (0, 0))
+    x, y = pygame.mouse.get_pos()
+    x -= mouse_cursor.get_width() / 2
+    y -= mouse_cursor.get_height() / 2
+    screen.blit(mouse_cursor, (x, y))
+    pygame.display.update()
